087d169f89ef5cab87f6c8b60d17e05a43b9065a
st181578 <cagnazzo.christian_damiano@educ.di.unito.it>
2020-03-28 19:39:38 +0100

    correct indentation

--- Esercizio1/src/sorting_lib.c
+++ Esercizio1/src/sorting_lib.c
@@ -1,50 +1,50 @@
 #include "sorting_lib.h"
 
 static int partition(void ** array, SortingLibCmp compare, int first, int last){
-    int i = first+1;
-    int j = last;
-    while (i<=j){
-        if (compare(array[i],array[first]) <= 0)
-            i++;
-        else if (compare(array[j],array[first]) > 0)
-            j--;
-            else {
-                void * temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-                i++;
-                j--;
-            }
-    }
-    void * temp = array[j];
-    array[j] = array[first];
-    array[first] = temp;
-    return j;
+  int i = first+1;
+  int j = last;
+  while (i<=j){
+    if (compare(array[i],array[first]) <= 0)
+      i++;
+    else if (compare(array[j],array[first]) > 0)
+      j--;
+      else {
+        void * temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        i++;
+        j--;
+      }
+  }
+  void * temp = array[j];
+  array[j] = array[first];
+  array[first] = temp;
+  return j;
 }
 
 void Sorting_Lib_quick_sort(void ** array, SortingLibCmp compare, int first_index, int last_index){
-    if (last_index > 0){
-        int p = partition(array, compare, first_index, last_index);
-        if (p > 1)
-            Sorting_Lib_quick_sort(array, compare, 0, p-1);
-        if (p < last_index-1)
-            Sorting_Lib_quick_sort(array, compare, p+1, last_index);
-    }
+  if (last_index > 0){
+    int p = partition(array, compare, first_index, last_index);
+    if (p > 1)
+      Sorting_Lib_quick_sort(array, compare, 0, p-1);
+    if (p < last_index-1)
+      Sorting_Lib_quick_sort(array, compare, p+1, last_index);
+  }
 }
 
 void Sorting_Lib_insertion_sort(void ** array, SortingLibCmp compare, int num_elements){
-    int i,j;
-    void *temp;
+  int i,j;
+  void *temp;
 
-    for (i = 1; i < num_elements; i++){ 
-        j = i;
-        while (j>0 && compare(array[j],array[j-1]) < 0){
-            temp = array[j];
-            array[j] = array[j-1];
-            array[j-1] = temp;            
-            j--;
-        }
+  for (i = 1; i < num_elements; i++){ 
+    j = i;
+    while (j>0 && compare(array[j],array[j-1]) < 0){
+      temp = array[j];
+      array[j] = array[j-1];
+      array[j-1] = temp;            
+      j--;
     }
+  }
 }
 
 
--- Esercizio1/test/test_insertion_sort.c
+++ Esercizio1/test/test_insertion_sort.c
@@ -4,142 +4,142 @@
 #include "string.h"
 
 static int* new_int(int n) {
-    int* result = (int*) malloc(sizeof(int));
-    *result = n;
+  int* result = (int*) malloc(sizeof(int));
+  *result = n;
 
-    return result;
+  return result;
 }
 
 static char* new_char(char n) {
-    char* result = (char*) malloc(sizeof(char));
-    *result = n;
+  char* result = (char*) malloc(sizeof(char));
+  *result = n;
 
-    return result;
+  return result;
 }
 
 int compare_ints(int * a, int * b){ 
-    return * (int *) a - * (int *) b;
+  return * (int *) a - * (int *) b;
 }
 
 int compare_string(char * a, char * b){ 
-    return strcmp(a,b);
+  return strcmp(a,b);
 }
 
 void test_array_int_disordered(){
-    int ** array;
-    array = malloc(sizeof(int *)*3);
-    array[0] = new_int(3);
-    array[1] = new_int(7);
-    array[2] = new_int(2);
+  int ** array;
+  array = malloc(sizeof(int *)*3);
+  array[0] = new_int(3);
+  array[1] = new_int(7);
+  array[2] = new_int(2);
 
-    TEST_ASSERT_EQUAL(3, * (int *)array[0]);
-    TEST_ASSERT_EQUAL(7, * (int *)array[1]);
-    TEST_ASSERT_EQUAL(2, * (int *)array[2]);
+  TEST_ASSERT_EQUAL(3, * (int *)array[0]);
+  TEST_ASSERT_EQUAL(7, * (int *)array[1]);
+  TEST_ASSERT_EQUAL(2, * (int *)array[2]);
 
-    Sorting_Lib_insertion_sort((void*)array,(SortingLibCmp) compare_ints, 3);
+  Sorting_Lib_insertion_sort((void*)array,(SortingLibCmp) compare_ints, 3);
 
-    TEST_ASSERT_EQUAL(2, * (int *)array[0]);
-    TEST_ASSERT_EQUAL(3, * (int *)array[1]);
-    TEST_ASSERT_EQUAL(7, * (int *)array[2]);
+  TEST_ASSERT_EQUAL(2, * (int *)array[0]);
+  TEST_ASSERT_EQUAL(3, * (int *)array[1]);
+  TEST_ASSERT_EQUAL(7, * (int *)array[2]);
 
-    free(array);
+  free(array);
 }
 
 void test_grow_int_array(){
-    int ** array;
-    array = malloc(sizeof(int *)*3);
-    array[0] = new_int(3);
-    array[1] = new_int(4);
-    array[2] = new_int(5);
+  int ** array;
+  array = malloc(sizeof(int *)*3);
+  array[0] = new_int(3);
+  array[1] = new_int(4);
+  array[2] = new_int(5);
 
-    Sorting_Lib_insertion_sort((void*)array,(SortingLibCmp) compare_ints, 3);
+  Sorting_Lib_insertion_sort((void*)array,(SortingLibCmp) compare_ints, 3);
 
-    TEST_ASSERT_EQUAL(3, * (int *) array[0]);
-    TEST_ASSERT_EQUAL(4, * (int *) array[1]);
-    TEST_ASSERT_EQUAL(5, * (int *) array[2]);
+  TEST_ASSERT_EQUAL(3, * (int *) array[0]);
+  TEST_ASSERT_EQUAL(4, * (int *) array[1]);
+  TEST_ASSERT_EQUAL(5, * (int *) array[2]);
 
-    free(array);
+  free(array);
 }
 
 void test_desc_int_array(){
-    int ** array;
-    array = malloc(sizeof(int *)*3);
-    array[0] = new_int(5);
-    array[1] = new_int(4);
-    array[2] = new_int(3);
+  int ** array;
+  array = malloc(sizeof(int *)*3);
+  array[0] = new_int(5);
+  array[1] = new_int(4);
+  array[2] = new_int(3);
 
-    Sorting_Lib_insertion_sort((void*)array,(SortingLibCmp) compare_ints, 3);
+  Sorting_Lib_insertion_sort((void*)array,(SortingLibCmp) compare_ints, 3);
 
-    TEST_ASSERT_EQUAL(3, * (int *) array[0]);
-    TEST_ASSERT_EQUAL(4, * (int *) array[1]);
-    TEST_ASSERT_EQUAL(5, * (int *) array[2]);
+  TEST_ASSERT_EQUAL(3, * (int *) array[0]);
+  TEST_ASSERT_EQUAL(4, * (int *) array[1]);
+  TEST_ASSERT_EQUAL(5, * (int *) array[2]);
 
-    free(array);
+  free(array);
 }
 
 void test_one_int_array(){
-    int ** array;
-    array = malloc(sizeof(int *)*1);
-    array[0] = new_int(5);
-    
-    Sorting_Lib_insertion_sort((void*)array,(SortingLibCmp) compare_ints, 1);
+  int ** array;
+  array = malloc(sizeof(int *)*1);
+  array[0] = new_int(5);
+  
+  Sorting_Lib_insertion_sort((void*)array,(SortingLibCmp) compare_ints, 1);
 
-    TEST_ASSERT_EQUAL(5, * (int *) array[0]);
-    
-    free(array);
+  TEST_ASSERT_EQUAL(5, * (int *) array[0]);
+  
+  free(array);
 }
 
 void test_array_int_equals(){
-    int ** array;
-    array = malloc(sizeof(int *)*3);
-    array[0] = new_int(3);
-    array[1] = new_int(3);
-    array[2] = new_int(3);
-    
-    Sorting_Lib_insertion_sort((void*)array,(SortingLibCmp) compare_ints, 3);
+  int ** array;
+  array = malloc(sizeof(int *)*3);
+  array[0] = new_int(3);
+  array[1] = new_int(3);
+  array[2] = new_int(3);
+  
+  Sorting_Lib_insertion_sort((void*)array,(SortingLibCmp) compare_ints, 3);
 
-    TEST_ASSERT_EQUAL(3, * (int *)array[0]);
-    TEST_ASSERT_EQUAL(3, * (int *)array[1]);
-    TEST_ASSERT_EQUAL(3, * (int *)array[2]);
-    
-    free(array);
+  TEST_ASSERT_EQUAL(3, * (int *)array[0]);
+  TEST_ASSERT_EQUAL(3, * (int *)array[1]);
+  TEST_ASSERT_EQUAL(3, * (int *)array[2]);
+  
+  free(array);
 }
 
 void test_array_char(){
-    char ** a = malloc(sizeof(char)*3);
-    a[0] = new_char('C');
-    a[1] = new_char('O');
-    a[2] = new_char('A');
-    
-    Sorting_Lib_insertion_sort((void*)a,(SortingLibCmp) compare_string, 3);
+  char ** a = malloc(sizeof(char)*3);
+  a[0] = new_char('C');
+  a[1] = new_char('O');
+  a[2] = new_char('A');
+  
+  Sorting_Lib_insertion_sort((void*)a,(SortingLibCmp) compare_string, 3);
 
-    TEST_ASSERT_EQUAL('A',* (char *)a[0]);
-    TEST_ASSERT_EQUAL('C',* (char *)a[1]);
-    TEST_ASSERT_EQUAL('O',* (char *)a[2]);
+  TEST_ASSERT_EQUAL('A',* (char *)a[0]);
+  TEST_ASSERT_EQUAL('C',* (char *)a[1]);
+  TEST_ASSERT_EQUAL('O',* (char *)a[2]);
 
-    free(a);
+  free(a);
 }
 
 void test_empty_array(){
-    char ** a = NULL;
+  char ** a = NULL;
 
-    Sorting_Lib_insertion_sort((void *)a, (SortingLibCmp) compare_string,0);
+  Sorting_Lib_insertion_sort((void *)a, (SortingLibCmp) compare_string,0);
 
-    TEST_ASSERT_EQUAL(NULL, a);
+  TEST_ASSERT_EQUAL(NULL, a);
 }
 
 
 int main(){
-    UNITY_BEGIN();
+  UNITY_BEGIN();
 
-    RUN_TEST(test_array_int_disordered);
-    RUN_TEST(test_grow_int_array);
-    RUN_TEST(test_desc_int_array);
-    RUN_TEST(test_one_int_array);
-    RUN_TEST(test_array_char);
-    RUN_TEST(test_array_int_equals);
-    RUN_TEST(test_empty_array);
+  RUN_TEST(test_array_int_disordered);
+  RUN_TEST(test_grow_int_array);
+  RUN_TEST(test_desc_int_array);
+  RUN_TEST(test_one_int_array);
+  RUN_TEST(test_array_char);
+  RUN_TEST(test_array_int_equals);
+  RUN_TEST(test_empty_array);
 
-    UNITY_END();
+  UNITY_END();
 
 }
