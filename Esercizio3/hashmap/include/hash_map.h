096f403f81963ae2419207ad7348ba57a2f86b73
st181578 <cagnazzo.christian_damiano@educ.di.unito.it>
2020-04-14 19:48:04 +0200

    add method new hash map, insert and remove element

--- Esercizio3/hashmap/include/hash_map.h
+++ Esercizio3/hashmap/include/hash_map.h
@@ -1,27 +1,27 @@
-#pragma once;
+#pragma once
 
 typedef struct _Node Node;
-typedef struct _Table HashMap;
+typedef struct _HashMap HashMap;
 
 typedef int (*HashFunction)(void *);
 
-HashMap * HashMap_new();
+HashMap * HashMap_new(HashFunction, int);
 
 void HashMap_free(HashMap *);
 
 int HashMap_is_empty(HashMap *);
+
+void HashMap_insert(HashMap *, void *, void *);
+
+void HashMap_delete(HashMap *, void *);
+
+void * HashMap_get(HashMap *, void *);
 
 int HashMap_number_associations(HashMap *);
 
 void HashMap_delete_all_associations(HashMap *);
 
 int HashMap_key_is_present(HashMap *, void *);
-
-void HashMap_key_insert(HashMap *, void *, void *);
-
-void * HashMap_get(HashMap *, void *);
-
-void HashMap_delete(HashMap *, void *);
 
 void ** HashMap_all_key(HashMap *);
 
--- Esercizio3/hashmap/src/hash_map.c
+++ Esercizio3/hashmap/src/hash_map.c
@@ -1,18 +1,89 @@
-#include "hash_map.h";
-
-#define INITIAL_CAPACITY 100
+#include "hash_map.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 struct _Node {
   void * key;
   void * value;
   Node * next;
+  Node * prev;
 };
 
 struct _HashMap {
-  int size;
-  int capacity;
+  int num_elemets;
+  int table_capacity;
   Node ** table;
+  HashFunction hash_fun;
 };
+
+HashMap * HashMap_new(HashFunction hash_fun, int capacity){  /* capacità utente?? */
+  HashMap * result = (HashMap*) malloc(sizeof(HashMap));
+  result->num_elemets = 0;
+  result->table_capacity = capacity;
+  void ** table = (void**) malloc(sizeof(void*)*result->table_capacity);
+  for (int i = 0; i < result->table_capacity; i++) 
+    table[i] = NULL;
+  result->table = table;
+  result->hash_fun = hash_fun;
+  return result;
+}
+
+void HashMap_free(HashMap * hash_map){ /* ?? */
+  free(hash_map->table);
+  free(hash_map);
+}
+
+int HashMap_is_empty(HashMap * hash_map){
+  return !(hash_map->num_elemets);
+}
+
+void HashMap_insert(HashMap * hash_map, void * key, void * value){ /* controllo se c'è gia?? */
+  int pos = hash_map->hash_fun(key) % hash_map->table_capacity;
+  
+  if (hash_map->table[pos] == NULL){
+    Node * result = (Node*) malloc(sizeof(Node));
+    result->key = key;
+    result->value = value;
+    result->next = NULL;
+    result->prev = NULL;
+    hash_map->table[pos] = result;
+  } 
+  else {
+    Node * result = (Node*) malloc(sizeof(Node));
+    result->key = key;
+    result->value = value;
+    result->next = hash_map->table[pos];
+    result->prev = NULL;
+    result->next->prev = result;
+    hash_map->table[pos] = result;  
+  }
+
+  hash_map->num_elemets++;
+}
+
+void HashMap_delete(HashMap * hash_map, void * key){ /* funzione di comparizione */
+  int pos = hash_map->hash_fun(key) % hash_map->table_capacity;
+
+  if (hash_map->table[pos] != NULL){
+    Node * current = hash_map->table[pos];
+    while (current != NULL && current->key != key)
+      current = current->next;
+    if (current != NULL){
+      if (current->prev != NULL)
+        current->prev->next = current->next;
+      else
+        hash_map->table[pos] = current->next;  
+      if (current->next != NULL)  
+        current->next->prev = current->prev;
+      free(current);
+      hash_map->num_elemets--;
+    }
+  }
+}
+
+
+
+
 
 
 
